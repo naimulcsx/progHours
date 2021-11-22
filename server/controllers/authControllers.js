7fe1744bf64de2dbe301020c5ed0ac4a285a26e0
Naimul Haque <naimulcsx@gmail.com>
2021-11-22 19:12:28 +0600

    Add auth controllers

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,20 @@
+const { User } = require("../models")
+
+exports.register = async (req, res) => {
+  const { name, password, uid, email } = req.body
+  try {
+    const newUser = await User.create({
+      name,
+      password,
+      uid,
+      email,
+    })
+    res.json(newUser)
+  } catch (err) {
+    res.json(err)
+  }
+}
+
+exports.login = async (req, res) => {
+  res.send("Login route")
+}
