35156b03ac7c2a0e9af576a21be209734a60f3fd
Naimul Haque <naimulcsx@gmail.com>
2021-11-27 15:50:09 +0600

    Add clearAuthData utility

---
+++ client/src/utils/clearAuthData.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+const clearAuthData = () => {
+  // remove data from localStorage
+  localStorage.removeItem("isLoggedIn")
+  localStorage.removeItem("role")
+  // remove the accessToken cookie
+  return axios.get("/api/auth/logout")
+}
+
+export default clearAuthData
