a3ce0e5a37690f9c0b28320f1197b91f237cd8d3
Naimul Haque <naimulcsx@gmail.com>
2021-11-27 15:51:48 +0600

    Add logout button

--- client/src/pages/dashboard/Home.js
+++ client/src/pages/dashboard/Home.js
@@ -1,5 +1,20 @@
+import { useNavigate } from "react-router"
+import { toast } from "react-toastify"
+import clearAuthData from "utils/clearAuthData"
+
 const DashboardHome = () => {
-  return <div>Dashboard Home</div>
+  const navigate = useNavigate()
+  const handleLogout = async () => {
+    await clearAuthData()
+    toast.success("Logged out", { className: "toast" })
+    navigate("/login")
+  }
+  return (
+    <div>
+      <p>Welcome to secrect page</p>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  )
 }
 
 export default DashboardHome
