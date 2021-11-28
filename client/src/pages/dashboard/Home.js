161fb7717dce0d23e96f1401759c8d01d480fefe
Naimul Haque <naimulcsx@gmail.com>
2021-11-28 21:21:03 +0600

    Clear dashboard dummy texts

--- client/src/pages/dashboard/Home.js
+++ client/src/pages/dashboard/Home.js
@@ -1,21 +1,7 @@
 import Dashboardlayout from "components/DashboardLayout"
-import { useNavigate } from "react-router"
-import { toast } from "react-toastify"
-import clearAuthData from "utils/clearAuthData"
 
 const DashboardHome = () => {
-  const navigate = useNavigate()
-  const handleLogout = async () => {
-    await clearAuthData()
-    toast.success("Logged out", { className: "toast" })
-    navigate("/login")
-  }
-  return (
-    <Dashboardlayout>
-      <p>Welcome to secrect page</p>
-      <button onClick={handleLogout}>Logout</button>
-    </Dashboardlayout>
-  )
+  return <Dashboardlayout></Dashboardlayout>
 }
 
 export default DashboardHome
