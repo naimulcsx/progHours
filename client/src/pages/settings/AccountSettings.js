3abd27a5548dd564405221358fbefc94693994be
shahrierQA <shahrierfahim34@gmail.com>
2021-11-29 12:10:51 +0600

    enhance settings pages

--- client/src/pages/settings/AccountSettings.js
+++ client/src/pages/settings/AccountSettings.js
@@ -1,15 +1,86 @@
 import SettingsLayout from "components/SettingsLayout"
 
+// const settingsSchema = Yup.object().shape({
+//   uuid: Yup.string().trim(),
+//   name: Yup.string().trim(),
+//   password: Yup.string().trim().min(8, "Password must be 8 characters long"),
+//   email: Yup.string().email("Invalid email"),
+// })
+
+// const InputBox = ({ label }) => {
+//   return (
+//     <div className="flex-1 space-y-2">
+//       <h6 className="font-semibold pb-2">{label}</h6>
+//       <input className="input-box" />
+//     </div>
+//   )
+// }
+
 const AccountSettings = () => {
   return (
     <SettingsLayout>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum expedita
-      numquam inventore? Quod voluptatum voluptates quaerat quibusdam at
-      accusamus odio eos illum sed, fugiat, exercitationem ea doloribus!
-      Quibusdam, fugit magnam. Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Aut quia illum porro at eaque. Beatae a, perspiciatis
-      laudantium provident eveniet commodi expedita consequuntur officia nam ex
-      recusandae unde odit vel.
+      <h3>Account Settings</h3>
+
+      <div className="">
+        <form className="settings-form">
+          <div className="flex-1 space-y-2">
+            <h6 className="font-semibold pb-2">University ID</h6>
+            <input className="input-box" />
+          </div>
+
+          <div className="flex justify-end mt-4">
+            <button type="submit" className="btn-outline-primary py-1">
+              Save
+            </button>
+          </div>
+        </form>
+
+        <form className="settings-form">
+          <div className="flex-1 space-y-1">
+            <h6 className="font-semibold pb-2">
+              Please enter your full name, or a display name you are comfortable
+              with.
+            </h6>
+            <input className="input-box" />
+          </div>
+
+          <div className="flex justify-end mt-4">
+            <button type="submit" className="btn-outline-primary py-1">
+              Save
+            </button>
+          </div>
+        </form>
+
+        <form className="settings-form">
+          <div className="flex-1 space-y-2">
+            <h6 className="font-semibold pb-2">
+              Please enter the email address you want to use to log in
+            </h6>
+            <input className="input-box" />
+          </div>
+
+          <div className="flex justify-end mt-4">
+            <button type="submit" className="btn-outline-primary py-1">
+              Save
+            </button>
+          </div>
+        </form>
+
+        <form className="settings-form">
+          <div className="flex-1 space-y-2">
+            <h6 className="font-semibold pb-2">
+              Please enter the password you want to use to log in
+            </h6>
+            <input className="input-box" />
+          </div>
+
+          <div className="flex justify-end mt-4">
+            <button type="submit" className="btn-outline-primary py-1">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
     </SettingsLayout>
   )
 }
--- client/src/pages/settings/Profile.js
+++ client/src/pages/settings/Profile.js
@@ -1,12 +1,94 @@
 import SettingsLayout from "components/SettingsLayout"
+import InputField from "components/InputField"
+import Avatar from "react-avatar"
+import { useFormik } from "formik"
+import * as Yup from "yup"
+
+const profileFormSchema = Yup.object().shape({
+  name: Yup.string().trim().required("Name is required"),
+  uid: Yup.string()
+    .trim()
+    .required("University ID is required")
+    .length(7, "Invalid University ID"),
+
+  codeforces: Yup.string().trim(),
+  codechef: Yup.string(),
+  uva: Yup.string(),
+  spoj: Yup.string(),
+  atcoder: Yup.string(),
+})
 
 const EditProfile = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      uid: "",
+      codeforces: "",
+      codechef: "",
+      uva: "",
+      spoj: "",
+      atcoder: "",
+    },
+    validationSchema: profileFormSchema,
+
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
+
+  const hasError = (field) => formik.touched[field] && formik.errors[field]
+
   return (
     <SettingsLayout>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum expedita
-      numquam inventore? Quod voluptatum voluptates quaerat quibusdam at
-      accusamus odio eos illum sed, fugiat, exercitationem ea doloribus!
-      Quibusdam, fugit magnam.
+      <div className="grid items-start grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <h3>Edit your profile</h3>
+          <form className="mt-10 space-y-12" onSubmit={formik.handleSubmit}>
+            <InputField
+              id="name"
+              label="Full Name"
+              formik={formik}
+              haserror={hasError("name")}
+              msg={formik.errors.name}
+            />
+
+            <InputField
+              id="uid"
+              label="University ID"
+              formik={formik}
+              haserror={hasError("uid")}
+              msg={formik.errors.uid}
+            />
+
+            <h4 className="my-8 text-mainDark">Online Judge Handles</h4>
+
+            <InputField
+              id="codeforces"
+              formik={formik}
+              label="Codeforces Handle"
+            />
+
+            <InputField id="codechef" formik={formik} label="CodeChef Handle" />
+
+            <InputField id="uva" formik={formik} label="UVa Handle" />
+
+            <InputField id="spoj" formik={formik} label="SPOJ Handle" />
+
+            <InputField id="atcoder" formik={formik} label="AtCoder Handle" />
+
+            <div className="flex items-center space-x-6">
+              <button className="btn-outline-primary py-3" type="button">
+                Cancel
+              </button>
+              <button className="btn-primary" type="submit">
+                Save Changes
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <Avatar name="Fahim Shahrier" size={210} color="#5542F6" round={true} />
+      </div>
     </SettingsLayout>
   )
 }
