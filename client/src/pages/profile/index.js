1dd4a7982729495a95f03a4195243ee917b377f1
Naimul Haque <naimulcsx@gmail.com>
2021-12-04 19:51:59 +0600

    Update profile page

--- client/src/pages/profile/index.js
+++ client/src/pages/profile/index.js
@@ -2,20 +2,19 @@ import Navbar from "components/Navbar"
 import ProgressBox from "components/ProgressBox"
 
 export default function Profile() {
+  const name = localStorage.getItem("name")
   return (
     <div className="">
       <Navbar />
-      <div className="bg-primary flex items-center justify-center py-48 relative">
-        <div className="text-center space-y-3 text-white">
-          <h1 className="h1">Fahim Shahrier</h1>
-          <h3 className="h3">C181059</h3>
+      <div className="relative flex items-center justify-center py-48 bg-primary">
+        <div className="space-y-3 text-center">
+          <h1 className="text-white">{name}</h1>
+          <h3 className="text-white">C181065</h3>
         </div>
       </div>
-
       <div className="relative -top-20">
         <ProgressBox />
       </div>
-
       {/* tracking problem table */}
     </div>
   )
