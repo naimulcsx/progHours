5817e1dd73871fdedc440e32a09dab95f5085def
Naimul Haque <naimulcsx@gmail.com>
2021-12-04 18:37:38 +0600

    Restructure profile settings

--- client/src/pages/settings/AccountSettings.js
+++ client/src/pages/settings/AccountSettings.js
@@ -4,62 +4,27 @@ import InputField from "components/InputField"
 const AccountSettings = () => {
   return (
     <SettingsLayout>
-      <h3>Account Settings</h3>
+      <h3>Change Account Settings</h3>
 
       <div className="">
-        <div className="settings-form">
-          <div className="flex-1 space-y-2">
-            <InputField id="uuid"  label="University ID" />
-          </div>
+        <InputField id="email" label="Email" />
 
-          <div className="flex justify-end mt-4">
-            <button type="submit" className="btn-outline-primary py-1">
-              Save
-            </button>
-          </div>
+        <div className="flex justify-end mt-4">
+          <button type="submit" className="py-1 btn-outline-primary">
+            Save
+          </button>
         </div>
 
         <div className="settings-form">
-          <div className="flex-1 space-y-1">
-            <h6 className="font-semibold pb-2">
-              Please enter your full name, or a display name you are comfortable
-              with.
-            </h6>
-            <InputField id="name"  label="Full Name" />
+          <h3 className="font-semibold">Change your Password</h3>
+          <div className="flex-1 mt-12 space-y-8">
+            <InputField id="curr-pass" label="Current Password" />
+            <InputField id="new-pass" label="New Password" />
+            <InputField id="confirm-pass" label="Confirm Password" />
           </div>
 
           <div className="flex justify-end mt-4">
-            <button type="submit" className="btn-outline-primary py-1">
-              Save
-            </button>
-          </div>
-        </div>
-
-        <div className="settings-form">
-          <div className="flex-1 space-y-2">
-            <h6 className="font-semibold pb-2">
-              Please enter the email address you want to use to log in
-            </h6>
-            <InputField id="email"  label="Email" />
-          </div>
-
-          <div className="flex justify-end mt-4">
-            <button type="submit" className="btn-outline-primary py-1">
-              Save
-            </button>
-          </div>
-        </div>
-
-        <div className="settings-form">
-          <h4 className="font-semibold">Change your Password</h4>
-          <div className="flex-1 space-y-8 mt-12">
-            <InputField id="curr-pass"  label="Current Password" />
-            <InputField id="new-pass"  label="New Password" />
-            <InputField id="confirm-pass"  label="Confirm Password" />
-          </div>
-
-          <div className="flex justify-end mt-4">
-            <button type="submit" className="btn-outline-primary py-1">
+            <button type="submit" className="py-1 btn-outline-primary">
               Save
             </button>
           </div>
--- client/src/pages/settings/Profile.js
+++ client/src/pages/settings/Profile.js
@@ -10,7 +10,6 @@ const profileFormSchema = Yup.object().shape({
     .trim()
     .required("University ID is required")
     .length(7, "Invalid University ID"),
-
   codeforces: Yup.string().trim(),
   codechef: Yup.string(),
   uva: Yup.string(),
@@ -42,42 +41,47 @@ const EditProfile = () => {
     <SettingsLayout>
       <div className="grid items-start grid-cols-3 gap-8">
         <div className="col-span-2">
-          <h3>Edit your profile</h3>
           <form className="mt-10 space-y-12" onSubmit={formik.handleSubmit}>
-            <InputField
-              id="name"
-              label="Full Name"
-              formik={formik}
-              haserror={hasError("name")}
-              msg={formik.errors.name}
-            />
+            {/* edit profile: for name, email and uid  */}
+            <div className="space-y-4">
+              <h3 className="mb-8">Edit your profile</h3>
+              <InputField
+                id="name"
+                label="Full Name"
+                formik={formik}
+                haserror={hasError("name")}
+                msg={formik.errors.name}
+              />
+              <InputField
+                id="uid"
+                label="University ID"
+                formik={formik}
+                haserror={hasError("uid")}
+                msg={formik.errors.uid}
+              />
+            </div>
 
-            <InputField
-              id="uid"
-              label="University ID"
-              formik={formik}
-              haserror={hasError("uid")}
-              msg={formik.errors.uid}
-            />
+            {/* edit profile: online judge handles */}
+            <div className="space-y-4">
+              <h3 className="mb-8">Online Judge Handles</h3>
+              <InputField
+                id="codeforces"
+                formik={formik}
+                label="Codeforces Handle"
+              />
+              <InputField
+                id="codechef"
+                formik={formik}
+                label="CodeChef Handle"
+              />
+              <InputField id="uva" formik={formik} label="UVa Handle" />
+              <InputField id="spoj" formik={formik} label="SPOJ Handle" />
+              <InputField id="atcoder" formik={formik} label="AtCoder Handle" />
+            </div>
 
-            <h4 className="my-8 text-mainDark">Online Judge Handles</h4>
-
-            <InputField
-              id="codeforces"
-              formik={formik}
-              label="Codeforces Handle"
-            />
-
-            <InputField id="codechef" formik={formik} label="CodeChef Handle" />
-
-            <InputField id="uva" formik={formik} label="UVa Handle" />
-
-            <InputField id="spoj" formik={formik} label="SPOJ Handle" />
-
-            <InputField id="atcoder" formik={formik} label="AtCoder Handle" />
-
+            {/* save buttons */}
             <div className="flex items-center space-x-6">
-              <button className="btn-outline-primary py-3" type="button">
+              <button className="py-3 btn-outline-primary" type="button">
                 Cancel
               </button>
               <button className="btn-primary" type="submit">
@@ -86,7 +90,6 @@ const EditProfile = () => {
             </div>
           </form>
         </div>
-
         <Avatar name="Fahim Shahrier" size={210} color="#5542F6" round={true} />
       </div>
     </SettingsLayout>
