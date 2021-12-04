520b5034fe7ac2c0515186f6ffc5221e92c2f522
Naimul Haque <naimulcsx@gmail.com>
2021-12-04 19:43:30 +0600

    Update spacing on dropdown menu

--- client/src/components/DropdownMenu.jsx
+++ client/src/components/DropdownMenu.jsx
@@ -15,9 +15,9 @@ const DropdownMenu = () => {
       {active && (
         <div className="absolute right-0 z-50 bg-white border border-gray-200 rounded-lg shadow-lg top-10">
           <ul className="text-gray-600 divide-y divide-gray-200">
-            <li className="px-8 py-3">Profile</li>
-            <li className="px-8 py-3">Settings</li>
-            <li className="px-8 py-3">Logout</li>
+            <li className="px-6 py-3">Profile</li>
+            <li className="px-6 py-3">Settings</li>
+            <li className="px-6 py-3">Logout</li>
           </ul>
         </div>
       )}
