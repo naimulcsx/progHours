01a89561e0d42f82be1036c09cc8d96b50d692d0
Naimul Haque <naimulcsx@gmail.com>
2021-12-11 11:29:10 +0600

    Refactor tracking components

---
+++ client/src/components/tracking/Table.jsx
@@ -0,0 +1,46 @@
+import TableRow from "components/tracking/TableRow"
+
+const problemStats = [
+  {
+    pid: "CF-1366C",
+    problemName: "Palindromic Paths",
+    judgeName: "CODEFORCES",
+    verdict: "AC",
+    solveTime: 54,
+    tags: ["Number Theory", "Greedy"],
+  },
+  {
+    pid: "CF-1366C",
+    problemName: "Palindromic Paths",
+    judgeName: "CODEFORCES",
+    verdict: "AC",
+    solveTime: 54,
+    tags: ["Number Theory", "Greedy"],
+  },
+]
+
+const TrackingTable = () => {
+  return (
+    <div className="pt-12">
+      <table>
+        <thead>
+          <tr className="text-base font-medium">
+            <td>ID</td>
+            <td>Problem Name</td>
+            <td>Verdict</td>
+            <td>Solve Time</td>
+            <td>Tags</td>
+            <td>Date</td>
+          </tr>
+        </thead>
+        <tbody>
+          {problemStats.map((problem, index) => {
+            return <TableRow problem={problem} />
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default TrackingTable
