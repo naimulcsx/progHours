a3e02f528cb281659b6211f1d909356133b19938
Naimul Haque <naimulcsx@gmail.com>
2022-01-30 21:36:16 +0600

    Enhance User Interface

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,17 +6,24 @@ import DropdownMenu from "./DropdownMenu"
 const Navbar = () => {
   const name = localStorage.getItem("name")
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between px-6 py-4">
+    <header className="fixed top-0 left-0 right-0 z-[9999] flex items-center justify-between px-6 py-3 bg-[#0e171f]">
       <Link to="/dashboard">
-        <Logo />
+        <Logo className="text-white" />
       </Link>
       <nav>
-        <ul className="flex items-center space-x-5">
+        <ul className="flex items-center justify-end space-x-3">
           <li className="flex items-center">
-            <Link to="/profile">
-              <Avatar name={name} size={36} color="#5542F6" round />
+            <Link to="/profile" className="flex items-center space-x-2">
+              <img
+                src={`https://robohash.org/${name}?bgset=bg2&size=48x48`}
+                alt={name}
+                className="rounded-full w-7 h-7"
+              />
+              <span className="text-sm text-white">{name}</span>
             </Link>
-            <DropdownMenu></DropdownMenu>
+          </li>
+          <li className="flex items-center">
+            <DropdownMenu />
           </li>
         </ul>
       </nav>
--- client/src/pages/dashboard/Tracking.js
+++ client/src/pages/dashboard/Tracking.js
@@ -9,34 +9,32 @@ export default function TrackingSheet() {
   const query = useQuery("practice", getSubmissions)
   return (
     <Dashboardlayout>
-      <div className="pt-28">
-        {/* tracking table header */}
-        <div className="flex items-center justify-between">
-          <h3>Tracking Sheet</h3>
-          <div className="flex items-center space-x-5">
-            <div className="flex items-center text-primary">
-              <div className="p-2 border border-r-0 border-lightGrey rounded-l-md">
-                <GridViewIcon size={20} />
-              </div>
-              <div className="p-2 border border-lightGrey rounded-r-md">
-                <ListViewIcon size={20} />
-              </div>
+      {/* tracking table header */}
+      <div className="flex items-center justify-between">
+        <h3 className="font-bold">Tracking Sheet</h3>
+        <div className="flex items-center space-x-5">
+          <div className="flex items-center text-primary">
+            <div className="p-2 border border-r-0 border-lightGrey rounded-l-md">
+              <GridViewIcon size={20} />
             </div>
-            <Link to="/submissions/new">
-              <button
-                type="button"
-                className="inline-flex items-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <PlusIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
-                New Entry
-              </button>
-            </Link>
+            <div className="p-2 border border-lightGrey rounded-r-md">
+              <ListViewIcon size={20} />
+            </div>
           </div>
+          <Link to="/submissions/new">
+            <button
+              type="button"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              <PlusIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
+              New Entry
+            </button>
+          </Link>
         </div>
-
-        {/* tracking table */}
-        {query.isSuccess && <TrackingTable problemData={query.data.data} />}
       </div>
+
+      {/* tracking table */}
+      {query.isSuccess && <TrackingTable problemData={query.data.data} />}
     </Dashboardlayout>
   )
 }
