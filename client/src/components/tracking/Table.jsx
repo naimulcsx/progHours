e2b28a422785aec5392e36e15248724a142fd480
Naimul Haque <naimulcsx@gmail.com>
2022-01-30 16:10:51 +0600

    Add pagination

--- client/src/components/tracking/Table.jsx
+++ client/src/components/tracking/Table.jsx
@@ -1,6 +1,12 @@
-import { useTable } from "react-table"
+import { usePagination, useTable } from "react-table"
 import { PlusIcon, TrashIcon, PencilAltIcon } from "@heroicons/react/outline"
 import { Link } from "react-router-dom"
+import {
+  FiChevronRight,
+  FiChevronLeft,
+  FiChevronsLeft,
+  FiChevronsRight,
+} from "react-icons/fi"
 
 const practiceColumns = [
   {
@@ -39,12 +45,32 @@ const practiceColumns = [
 ]
 
 const TrackingTable = ({ problemData }) => {
-  const tableInstance = useTable({
-    data: problemData,
-    columns: practiceColumns,
-  })
-  const { getTableProps, getTableBodyProps, rows, prepareRow, headerGroups } =
-    tableInstance
+  const tableInstance = useTable(
+    {
+      data: problemData,
+      columns: practiceColumns,
+      initialState: {
+        pageSize: 10,
+      },
+    },
+    usePagination
+  )
+  const {
+    getTableProps,
+    getTableBodyProps,
+    page,
+    prepareRow,
+    headerGroups,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = tableInstance
 
   if (problemData.length === 0)
     return (
@@ -105,14 +131,26 @@ const TrackingTable = ({ problemData }) => {
           })}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
+          {page.map((row) => {
             prepareRow(row)
             return (
-              <tr {...row.getRowProps()} key={row.id} className="bg-white">
+              <tr
+                {...row.getRowProps()}
+                key={row.id}
+                className="bg-white hover:bg-primary hover:bg-opacity-5"
+              >
                 {row.cells.map((cell) => {
+                  const extraProps = {}
+                  extraProps[`data-${cell.column.id.toLowerCase()}`] =
+                    cell.value
+
                   return (
-                    <td {...cell.getCellProps()} className="px-6 py-4">
-                      {cell.render("Cell")}
+                    <td
+                      {...cell.getCellProps()}
+                      {...extraProps}
+                      className="px-6 py-4"
+                    >
+                      <span>{cell.render("Cell")}</span>
                     </td>
                   )
                 })}
@@ -121,6 +159,58 @@ const TrackingTable = ({ problemData }) => {
           })}
         </tbody>
       </table>
+      <div className="flex items-center justify-between px-6 py-3 space-x-4 bg-white pagination">
+        <div>
+          <span>
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <select
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value))
+            }}
+          >
+            {[5, 10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            <FiChevronsLeft size={20} />
+          </button>{" "}
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            <FiChevronLeft size={20} />
+          </button>{" "}
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            <FiChevronRight size={20} />
+          </button>{" "}
+          <button
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            <FiChevronsRight size={20} />
+          </button>{" "}
+          {/* <span>
+            | Go to page:{" "}
+            <input
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                gotoPage(page)
+              }}
+              style={{ width: "100px" }}
+            />
+          </span> */}
+        </div>
+      </div>
     </div>
   )
 }
