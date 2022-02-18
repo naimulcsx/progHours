9d64ea075bd2f010ebe91de8cd9759bbfa8564a6
Naimul Haque <naimulcsx@gmail.com>
2022-02-18 17:03:48 +0600

    Remove timestamps from online_judges table

--- server/models/OnlineJudge.js
+++ server/models/OnlineJudge.js
@@ -23,6 +23,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
+      timestamps: false,
       underscored: true,
       modelName: "OnlineJudge",
     }
--- server/seeders/20220125150739-online-judges.js
+++ server/seeders/20220125150739-online-judges.js
@@ -11,56 +11,38 @@ module.exports = {
         {
           id: 1,
           name: "Codeforces",
-          created_at: new Date(),
-          updated_at: new Date(),
         },
         {
           id: 2,
           name: "SPOJ",
-          created_at: new Date(),
-          updated_at: new Date(),
         },
         {
           id: 3,
           name: "CodeChef",
-          created_at: new Date(),
-          updated_at: new Date(),
         },
         {
           id: 4,
           name: "AtCoder",
-          created_at: new Date(),
-          updated_at: new Date(),
         },
         {
           id: 5,
           name: "HackerRank",
-          created_at: new Date(),
-          updated_at: new Date(),
         },
         {
           id: 6,
           name: "LightOJ",
-          created_at: new Date(),
-          updated_at: new Date(),
         },
         {
           id: 7,
           name: "UVA",
-          created_at: new Date(),
-          updated_at: new Date(),
         },
         {
           id: 8,
           name: "CSES",
-          created_at: new Date(),
-          updated_at: new Date(),
         },
         {
           id: 9,
           name: "Toph",
-          created_at: new Date(),
-          updated_at: new Date(),
         },
       ],
       {}
