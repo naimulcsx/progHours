90ec39d7ce0559ae3a1a94b1a9b9c0e46442abd7
shahrierQA <shahrierfahim34@gmail.com>
2022-02-19 18:28:20 +0600

    Tag controller problem

--- server/routes/problemRoutes.js
+++ server/routes/problemRoutes.js
@@ -1,8 +1,18 @@
 const { createProblemTag } = require("../controllers/tagControllers")
 const isAuthenticated = require("../middlewares/isAutheticated")
+const { Problem, PracticeSubmission, Tag, ProblemTag, UserProblemTag } =
+  require("../models").sequelize.models
 
 const router = require("express").Router()
 
 router.route("/:pid/tags").post(createProblemTag)
+
+router.route("/test-sub").get(async (req, res, next) => {
+  const tag = await ProblemTag.findAll()
+
+  res.send({ tag })
+
+  console.log("---------------", tag)
+})
 
 module.exports = router
