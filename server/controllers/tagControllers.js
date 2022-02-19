1324a12cbae3a4ece518bc0c72207dac07763064
Naimul Haque <naimulcsx@gmail.com>
2022-02-19 20:15:57 +0600

    Add proper relationship to entities

--- server/controllers/tagControllers.js
+++ server/controllers/tagControllers.js
@@ -1,48 +1,55 @@
-const { Problem, PracticeSubmission, Tag, ProblemTag, UserProblemTag } =
+const { User, Problem, PracticeSubmission, Tag, ProblemTag, UserProblemTag } =
   require("../models").sequelize.models
+
+/**
+ ** This controller creates a tag for a particular problem with `problemId`
+ */
 
 const createProblemTag = async (req, res, next) => {
   const { name } = req.body
-  const { pid } = req.params
-
-  const userId = 1
-
+  const { id } = req.params
+  const userId = req.user.id
   try {
-    const tag = await Tag.findOne({ where: { name } })
-
-    let problemTagId
-
-    if (!tag) {
-      const newTag = await Tag.create({ name })
-
-      const createPTag = await ProblemTag.create({
-        problemId: pid,
-        tagId: newTag.dataValues.id,
-      })
-
-      console.log("-------------------", createPTag)
-
-      problemTagId = createPTag.dataValues.problemId
-    } else {
-      const pTag = await ProblemTag.findOne({
-        where: { problemId: pid },
-      })
-
-      problemTagId = pTag.dataValues.id
-
-      console.log("-------------------", pTag)
-      console.log("-------------------", pTag.id)
-    }
-
-    const userPTag = await UserProblemTag.create({
-      userId,
-      problemTagId,
+    /**
+     ** Check if the tag we are trying to add already exists
+     ** If it exists then we don't need to create it again
+     ** So we will use findOrCreate
+     */
+    const [tag] = await Tag.findOrCreate({
+      where: { name },
+      defaults: { name },
     })
-
-    return res.send({ tag, userPTag })
+    await ProblemTag.findOrCreate({
+      where: { problemId: id, tagId: tag.dataValues.id },
+      defaults: { problemId: id, tagId: tag.dataValues.id },
+    })
+    const newProblemTag = await ProblemTag.findOne({
+      where: { problemId: id, tagId: tag.dataValues.id },
+      attributes: ["id"],
+    })
+    const [userProblemTag] = await UserProblemTag.findOrCreate({
+      where: { problemTagId: newProblemTag.id, userId },
+      defaults: { problemTagId: newProblemTag.id, userId },
+    })
+    return res.send({ status: "success", userProblemTag })
   } catch (err) {
     res.json(err)
   }
 }
+
+// const testController = async (req, res, next) => {
+//   let tagId = 10,
+//     problemId = 1
+//   const problemTag = await ProblemTag.findOne({
+//     where: { tagId, problemId },
+//     attributes: ["id"],
+//   })
+//   // Find out the users who added a particular tag for a particular problem
+//   const val = await UserProblemTag.findAll({
+//     where: { problemTagId: problemTag.id },
+//     include: User,
+//   })
+//   res.send(val)
+// }
 
 module.exports = { createProblemTag }
--- server/models/UserProblemTag.js
+++ server/models/UserProblemTag.js
@@ -9,6 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.User, {
+        foreignKey: "userId",
+      })
+      this.belongsTo(models.ProblemTag, {
+        foreignKey: "problemTagId",
+      })
     }
   }
   UserProblemTag.init(
