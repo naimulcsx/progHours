54ee47f66d60ff7001191aefa310c1a733a1fcd5
Naimul Haque <naimulcsx@gmail.com>
2022-02-22 13:42:23 +0600

    Implement login functionality

--- nest-server/src/auth/auth.service.ts
+++ nest-server/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { InsertResult, Repository } from 'typeorm';
 import { User } from './user.entity';
+const bcrypt = require('bcrypt');
 
 @Injectable()
 export class AuthService {
@@ -9,8 +10,14 @@ export class AuthService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
-  register(userData): Promise<User[]> {
-    const newUser = this.usersRepository.create(userData);
+  comparePassword(password, hashedPassword) {
+    return bcrypt.compare(password, hashedPassword);
+  }
+  getUser(username: string): Promise<User> {
+    return this.usersRepository.findOne({ username });
+  }
+  registerUser(userData): Promise<User[]> {
+    const newUser = this.usersRepository.create({ ...userData, role: 'user' });
     return this.usersRepository.save(newUser);
   }
 }
