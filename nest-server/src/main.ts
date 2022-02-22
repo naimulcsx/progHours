a3889dfa82ebada47bc2647ea55054475608a7fe
Naimul Haque <naimulcsx@gmail.com>
2022-02-22 19:07:33 +0600

    Update dependency

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
