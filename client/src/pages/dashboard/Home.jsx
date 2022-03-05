c999ade00621f0a283ca78517ad3355722144e94
Naimul Haque <naimulcsx@gmail.com>
2022-03-05 21:03:09 +0600

    Update dashboard

--- client/src/pages/dashboard/Home.jsx
+++ client/src/pages/dashboard/Home.jsx
@@ -2,10 +2,19 @@ import Layout from "@/components/dashboard/Layout"
 import { Stats } from "../../components/dashboard/Stats"
 
 const DashboardHome = () => {
+  const name = localStorage.getItem("name")
   return (
     <Layout>
-      <div className="grid grid-cols-3">
-        <div className="bg-white px-8 py-6 rounded-xl">
+      <div className="flex items-center justify-between">
+        <div>
+          <h3 className="font-bold">Hi! {name} </h3>
+          <p className="text-gray-500 mt-1">
+            Here’s what’s going on in your competitive programming journey!
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 mt-6">
+        <div className="bg-white px-8 py-6 border border-slate-100 rounded-lg">
           <Stats />
         </div>
       </div>
