44e20849b9a4be5e24ec44b045a7a5868f94c5fc
MD Nur Uddin Shohan <nuruddin.shohan@yahoo.com>
2022-03-24 14:59:52 +0600

    progress box responsive

--- client/src/components/MobileNav.tsx
+++ client/src/components/MobileNav.tsx
@@ -10,7 +10,7 @@ export default function MobileNav() {
   return (
     <div className="fixed bottom-0 left-0 right-0 z-60 bg-white py-2 md:hidden ">
       <nav>
-        <ul className="flex justify-between">
+        <ul className="flex justify-between px-8">
           <NavLink Icon={DashboardIcon} to="/dashboard"></NavLink>
           <NavLink Icon={TrackingIcon} to="/submissions"></NavLink>
           <NavLink Icon={LeaderboardIcon} to="/leaderboard"></NavLink>
