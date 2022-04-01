af3934f2b2064190d027d4811e75cd954d4304f7
shahrierQA <shahrierfahim34@gmail.com>
2022-04-01 10:29:14 +0600

    Add oj handle settings api on client

--- client/src/components/submissions/Table.tsx
+++ client/src/components/submissions/Table.tsx
@@ -105,7 +105,7 @@ const TrackingTable = ({ submissions }) => {
 
   return (
     <div className="relative">
-      <div className="mt-6 shadow shadow-primary/5 rounded-md">
+      <div className="mt-6 rounded-md shadow shadow-primary/5">
         <table {...getTableProps()} className="border-collapse ">
           <thead>
             {headerGroups.map((headerGroup) => {
--- client/src/pages/settings/HandleSettings.tsx
+++ client/src/pages/settings/HandleSettings.tsx
@@ -1,30 +1,57 @@
 import SettingsLayout from "@/components/SettingsLayout"
 import { useFormik } from "formik"
 import * as Yup from "yup"
-import { FormControl, Input, Label, ErrorMessage } from "@/components/Form"
+import { ErrorMessage, FormControl, Input, Label } from "@/components/Form"
 import { Helmet } from "react-helmet-async"
+import { useState } from "react"
+import { useMutation, useQuery, useQueryClient } from "react-query"
+import { createOJHandle, getAllHandles } from "@/api/handle"
+import showErrorToasts from "@/utils/showErrorToasts"
+import { toast } from "react-toastify"
 
 const profileSchema = Yup.object().shape({
-  codeforces: Yup.string().trim(),
-  codechef: Yup.string(),
-  uva: Yup.string(),
-  spoj: Yup.string(),
-  atcoder: Yup.string(),
+  handle: Yup.string().trim().required("handle name is required"),
+  onlineJudge: Yup.string().required("online judge is required"),
 })
 
 const EditProfile = () => {
+  const [handles, setHandles] = useState([])
+
   const formik = useFormik({
     initialValues: {
-      codeforces: "",
-      codechef: "",
-      uva: "",
-      spoj: "",
-      atcoder: "",
+      handle: "",
+      onlineJudge: "1",
     },
     validationSchema: profileSchema,
 
     onSubmit: (values) => {
-      console.log(values)
+      const val = {
+        handle: values.handle,
+        judge_id: parseInt(values.onlineJudge, 10),
+      }
+      mutate(val)
+    },
+  })
+
+  const client = useQueryClient()
+  client.invalidateQueries("handles")
+
+  const { mutate } = useMutation(createOJHandle, {
+    onSuccess: () => {
+      formik.resetForm()
+      toast.success("create successfully")
+    },
+
+    onError: (err: any) => {
+      showErrorToasts(err.response.data.message)
+    },
+  })
+
+  useQuery("handles", getAllHandles, {
+    refetchOnWindowFocus: false,
+
+    onSuccess: (data) => {
+      setHandles(data?.handles)
     },
   })
 
@@ -39,59 +66,65 @@ const EditProfile = () => {
         <div className="space-y-6">
           <h3 className="mb-8">Online Judge Handles</h3>
 
-          <FormControl>
-            <Input
-              type="text"
-              placeholder=" "
-              {...formik.getFieldProps("codeforces")}
-            />
-            <Label>Codeforces Handle</Label>
-          </FormControl>
+          <div className="space-y-6">
+            {handles.length > 0 ? (
+              handles.map((item) => (
+                <FormControl key={item.id}>
+                  <Input
+                    type="text"
+                    placeholder=" "
+                    value={item.handle}
+                    readOnly
+                  />
+                  <Label>{item.judge_id.name}</Label>
+                </FormControl>
+              ))
+            ) : (
+              <div></div>
+            )}
+          </div>
 
-          <FormControl>
-            <Input
-              type="text"
-              placeholder=" "
-              {...formik.getFieldProps("codechef")}
-            />
-            <Label>CodeChef Handle</Label>
-          </FormControl>
+          <div className="flex items-center pt-10 space-x-5">
+            <div className="flex-1">
+              <FormControl
+                isInvalid={formik.touched.handle && formik.errors.handle}
+              >
+                <Input
+                  type="text"
+                  placeholder=" "
+                  {...formik.getFieldProps("handle")}
+                />
+                <Label>Handle name</Label>
+                <ErrorMessage>{formik.errors.handle}</ErrorMessage>
+              </FormControl>
+            </div>
 
-          <FormControl>
-            <Input
-              type="text"
-              placeholder=" "
-              {...formik.getFieldProps("uva")}
-            />
-            <Label>UVa Handle</Label>
-          </FormControl>
-
-          <FormControl>
-            <Input
-              type="text"
-              placeholder=" "
-              {...formik.getFieldProps("spoj")}
-            />
-            <Label>SPOJ Handle</Label>
-          </FormControl>
-
-          <FormControl>
-            <Input
-              type="text"
-              placeholder=" "
-              {...formik.getFieldProps("atcoder")}
-            />
-            <Label>AtCoder Handle</Label>
-          </FormControl>
+            <FormControl
+              isInvalid={
+                formik.touched.onlineJudge && formik.errors.onlineJudge
+              }
+            >
+              <select
+                {...formik.getFieldProps("onlineJudge")}
+                className="w-full p-3 border-2 border-gray-200 rounded-md focus:outline-none"
+              >
+                <option value="1">CodeForces</option>
+                <option value="2">CodeChef</option>
+                <option value="3">CSES</option>
+                <option value="4">UVa</option>
+                <option value="5">Toph</option>
+                <option value="6">SPOJ</option>
+                <option value="7">Hackerrank</option>
+              </select>
+              <ErrorMessage>{formik.errors.onlineJudge}</ErrorMessage>
+            </FormControl>
+          </div>
         </div>
 
         {/* save buttons */}
         <div className="flex items-center justify-end space-x-6">
-          <button className="py-3 btn-outline" type="button">
-            Cancel
-          </button>
           <button className="btn-primary" type="submit">
-            Save Changes
+            Save
           </button>
         </div>
       </form>
