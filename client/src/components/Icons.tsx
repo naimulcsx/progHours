b985ae80d615c423a48d29819a77d25a8bef5964
Naimul Haque <naimulcsx@gmail.com>
2022-04-01 07:29:28 +0600

    Update icon for CF Gym

--- client/src/components/Icons.tsx
+++ client/src/components/Icons.tsx
@@ -388,8 +388,8 @@ function LightOJIcon() {
 function UVAIcon() {
   return (
     <svg
-      width={24}
-      height={24}
+      width={40}
+      height={40}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
