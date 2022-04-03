b5deabedde700edb22bc21f2bdf46a4fee78d12c
Naimul Haque <naimulcsx@gmail.com>
2022-04-03 12:36:19 +0600

    Update profile page

--- client/src/pages/profile/index.tsx
+++ client/src/pages/profile/index.tsx
@@ -1,24 +1,29 @@
 import Navbar from "@/components/Navbar"
 import ProgressBox from "@/components/ProgressBox"
+import { GlobalContext } from "@/GlobalStateProvider"
+import { useContext } from "react"
 import { useQuery } from "react-query"
-import { getStats } from "../../api/dashboard"
+import { getStats } from "@/api/dashboard"
 
 export default function Profile() {
-  const name = localStorage.getItem("name")
+  const { user } = useContext(GlobalContext)
   const query = useQuery("stats", getStats)
   return (
     <div className="">
-      <Navbar bgWhite />
-      <div className="relative flex items-center justify-center py-48 bg-primary">
-        <div className="space-y-3 text-center">
-          <h1 className="text-white">{name}</h1>
-          <h3 className="text-white">C181065</h3>
+      <Navbar />
+      <div className="relative flex items-center justify-center pt-32 pb-16">
+        <div className="space-y-6 text-center">
+          <img
+            src={`https://robohash.org/${user.name}?bgset=bg2&size=80x80`}
+            alt={user.name}
+            className="rounded-full mx-auto w-20 h-20"
+          />
+          <h1 className="text-4xl">{user.name}</h1>
+          <span className="text-2xl">{user.username}</span>
         </div>
       </div>
       <div className="container mx-auto px-6">
-        <div className="relative -top-20">
-          {query.data && <ProgressBox progress={query.data} />}
-        </div>
+        {query.data && <ProgressBox progress={query.data} />}
       </div>
       {/* tracking problem table */}
     </div>
