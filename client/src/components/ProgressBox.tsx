34dd0d7f723d63cc523cf0b968f54da25c914f14
MD Nur Uddin Shohan <nuruddin.shohan@yahoo.com>
2022-04-07 23:33:27 +0600

    progress bar fix

--- client/src/components/ProgressBox.tsx
+++ client/src/components/ProgressBox.tsx
@@ -28,7 +28,7 @@ interface Progress {
 const Box = ({ title, icon, data }: ProgressBox) => {
   return (
     <div
-      className="flex flex-col text-center items-center mx-auto px-8 py-5  bg-white rounded-md  shadow shadow-primary/5 w-full
+      className="flex flex-col text-center items-center mx-auto space-x-2 px-8 py-5  bg-white rounded-md  shadow shadow-primary/5 w-full
     md:flex-row md:items-start md:text-left md:mx-0 md:w-auto md:space-x-4
     "
     >
@@ -36,7 +36,7 @@ const Box = ({ title, icon, data }: ProgressBox) => {
         {icon}
       </div>
       <div className="pt-1 space-y-4">
-        <h4 className="text-dark font-medium text-lg">{title}</h4>
+        <h4 className="text-dark font-medium text-lg md:text-xl">{title}</h4>
         <div className="space-y-4">
           <h2 className="text-2xl md:text-3xl">{data}</h2>
         </div>
@@ -58,7 +58,7 @@ const ProgressBox = ({ progress }: { progress: Progress }) => {
   return (
     <div className="flex justify-center">
       <div className="w-full">
-        <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4 md:gap-8">
           <Box
             icon={<StarIcon className="w-7" />}
             title="Points"
