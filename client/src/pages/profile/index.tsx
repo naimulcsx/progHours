8ae061fa47fd824bd9cf3e0cdc7ccf0f09262ffb
Naimul Haque <naimulcsx@gmail.com>
2022-04-07 22:00:02 +0600

    update profile page

--- client/src/pages/profile/index.tsx
+++ client/src/pages/profile/index.tsx
@@ -1,21 +1,36 @@
+import { useContext, useState } from "react"
+import { useQuery } from "react-query"
+
+/**
+ * Import Components
+ */
 import MobileNav from "@/components/MobileNav"
 import Navbar from "@/components/Navbar"
 import ProgressBox from "@/components/ProgressBox"
 import { GlobalContext } from "@/GlobalStateProvider"
-import { useContext, useState } from "react"
-import { useQuery } from "react-query"
+import ProfileTable from "@/components/profile/Table"
+
+/**
+ * Import API
+ */
 import { getStats } from "@/api/dashboard"
 import { getSubmissions } from "@/api/submissions"
-import ProfileTable from "@/components/profile/Table"
 
 export default function Profile() {
   const { user } = useContext(GlobalContext)
+
+  /**
+   * Get statistics
+   */
   const progressQuery = useQuery("stats", getStats)
-  let [submissionList, setSubmissionList] = useState([])
-  const query = useQuery("submissionList", getSubmissions, {
+  let [submissions, setSubmissions] = useState([])
+
+  /**
+   * Get submissions
+   */
+  useQuery("submissionList", getSubmissions, {
     onSuccess: (data) => {
-      console.log(data.submissions)
-      setSubmissionList(data.submissions)
+      setSubmissions(data.submissions)
     },
   })
 
@@ -25,21 +40,19 @@ export default function Profile() {
       <div className="relative flex items-center justify-center pt-32 pb-16">
         <div className="space-y-6 text-center">
           <img
-            src={`https://robohash.org/${user.name}?bgset=bg2&size=80x80`}
-            alt={user.name}
+            src={`https://robohash.org/${user?.name}?bgset=bg2&size=80x80`}
+            alt={user?.name}
             className="rounded-full mx-auto w-20 h-20"
           />
-          <h1 className="text-4xl">{user.name}</h1>
-          <span className="text-2xl">{user.username}</span>
+          <h1 className="text-4xl">{user?.name}</h1>
+          <span className="text-2xl">{user?.username}</span>
         </div>
       </div>
       <div className="container mx-auto px-6 space-y-16">
         {progressQuery.data && <ProgressBox progress={progressQuery.data} />}
-        <ProfileTable submissions={submissionList} />
+        <ProfileTable submissions={submissions} />
       </div>
       <MobileNav></MobileNav>
-      {/* tracking problem table */}
     </div>
   )
 }
-//        <ProfileTable submissionList={submissionList} />
