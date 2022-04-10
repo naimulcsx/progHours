11645cf0cf5d35f785ff63621084fcf113d64cfb
Naimul Haque <naimulcsx@gmail.com>
2022-04-10 14:59:20 +0600

    Add Swagger UI (#68)

--- server/src/validators/create-user-dto.ts
+++ server/src/validators/create-user-dto.ts
@@ -1,15 +1,34 @@
 import { Matches, IsString, IsEmail, MinLength } from "class-validator"
+import { ApiProperty } from "@nestjs/swagger"
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: "Full name of the user",
+    example: "Naimul Haque",
+  })
   @IsString()
   name: string
 
+  @ApiProperty({
+    description: "Username of the user",
+    example: "C181065",
+    pattern: "^[C][0-9]{6}$",
+  })
   @Matches(/^[C][0-9]{6}$/, { message: "invalid uid" })
   username: string
 
+  @ApiProperty({
+    description: "Email of the user",
+    example: "naimulcsx@gmail.com",
+  })
   @IsEmail()
   email: string
 
+  @ApiProperty({
+    description: "Password of the user",
+    example: "YQ3jwr6ycN",
+    minLength: 8,
+  })
   @IsString()
   @MinLength(8)
   password: string
--- server/src/validators/login-user-dto.ts
+++ server/src/validators/login-user-dto.ts
@@ -1,9 +1,19 @@
 import { Matches, IsString } from "class-validator"
+import { ApiProperty } from "@nestjs/swagger"
 
 export class LoginUserDto {
+  @ApiProperty({
+    description: "Username of the user",
+    example: "C181065",
+    pattern: "^[C][0-9]{6}$",
+  })
   @Matches(/^[C][0-9]{6}$/, { message: "invalid uid" })
   username: string
 
+  @ApiProperty({
+    description: "Password of the user",
+    example: "YQ3jwr6ycN",
+  })
   @IsString()
   password: string
 }
