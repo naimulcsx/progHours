9ef5fac268675f9f8678756adc5366e28fb54a4e
Naimul Haque <naimulcsx@gmail.com>
2022-04-26 21:32:08 +0600

    Add input wizard

--- client/src/pages/dashboard/Tracking.tsx
+++ client/src/pages/dashboard/Tracking.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "react-query"
-import { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { Helmet } from "react-helmet-async"
 
 /**
@@ -14,11 +14,29 @@ import WeekFilters from "@/components/submissions/filters/WeekFilter"
  * Import helpers
  */
 import { GlobalContext } from "@/GlobalStateProvider"
+import { UploadIcon } from "@heroicons/react/outline"
+import ImportCsvModal from "./ImportCsvModal"
 
 export default function TrackingSheet() {
   const context = useContext(GlobalContext)
   const { query, filteredData, selectedWeek, setSelectedWeek, weekRanges } =
     context?.useSubmissionsResult
+
+  let [isOpen, setIsOpen] = useState(false)
+  const handleImport = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    console.log(files)
+    if (files) {
+      setIsOpen(true)
+      const reader = new FileReader()
+      reader.onload = function fileReadCompleted() {
+        // when the reader is done, the content is in reader.result.
+        console.log(reader.result)
+      }
+      reader.readAsText(files[0])
+    }
+    e.target.value = ""
+  }
 
   return (
     <Layout dataDependency={[query.data]}>
@@ -32,7 +50,7 @@ export default function TrackingSheet() {
             <Spinner show={query.isLoading || query.isRefetching} />
           </h3>
         </div>
-        <div className="mt-4">
+        <div className="flex justify-between mt-4">
           <ul className="flex items-center space-x-4">
             <li>
               <WeekFilters
@@ -40,6 +58,19 @@ export default function TrackingSheet() {
                 selected={selectedWeek}
                 setSelected={setSelectedWeek}
               />
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <label
+                htmlFor="csv-input"
+                className="flex items-center text-sm text-primary px-4 py-2 rounded-lg cursor-pointer border border-dashed border-primary"
+              >
+                <input type="file" id="csv-input" onInput={handleImport} />
+                <UploadIcon className="w-5 h-5 mr-2" />
+                Import via .csv
+              </label>
+              <ImportCsvModal isOpen={isOpen} setIsOpen={setIsOpen} />
             </li>
           </ul>
         </div>
