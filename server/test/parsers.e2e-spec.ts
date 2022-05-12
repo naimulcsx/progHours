6c5d77d54b17500761c163e9728c0e9ab2c28c23
Naimul Haque <naimulcsx@gmail.com>
2022-05-12 12:34:03 +0600

    Add e2e tests for parsers

---
+++ server/test/parsers.e2e-spec.ts
@@ -0,0 +1,34 @@
+import * as request from "supertest"
+import axios from "axios"
+
+const API_HOSTNAME = "http://localhost"
+
+describe("Parsers", () => {
+  test("CF Test 1", async () => {
+    const { data } = await axios.post(`${API_HOSTNAME}/api/parsers/test`, {
+      link: "https://codeforces.com/contest/1617/problem/A",
+    })
+    expect(data).toStrictEqual({
+      pid: "CF-1617A",
+      name: "Forbidden Subsequence",
+      tags: ["constructive algorithms", "greedy", "sortings", "strings"],
+      difficulty: 800,
+      judge_id: 1,
+      link: "https://codeforces.com/contest/1617/problem/A",
+    })
+  })
+
+  test("CF Test 2", async () => {
+    const { data } = await axios.post(`${API_HOSTNAME}/api/parsers/test`, {
+      link: "https://codeforces.com/problemset/problem/1617/A",
+    })
+    expect(data).toStrictEqual({
+      pid: "CF-1617A",
+      name: "Forbidden Subsequence",
+      tags: ["constructive algorithms", "greedy", "sortings", "strings"],
+      difficulty: 800,
+      judge_id: 1,
+      link: "https://codeforces.com/contest/1617/problem/A",
+    })
+  })
+})
