1914509873d7c0852ca6367ef94324f4e3f8d12c
Naimul Haque <naimulcsx@gmail.com>
2022-05-19 14:54:14 +0600

    Add 10s timeout to HTTP requests, resolves #156

--- server/src/modules/parsers/parsers.module.ts
+++ server/src/modules/parsers/parsers.module.ts
@@ -5,10 +5,10 @@ import { HttpModule } from "@nestjs/axios"
  * Import Services
  */
 import { ParsersService } from "@/modules/parsers/parsers.service"
-import { ParsersController } from './parsers.controller';
+import { ParsersController } from "./parsers.controller"
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule.register({ timeout: 10000 })],
   providers: [ParsersService],
   exports: [ParsersService],
   controllers: [ParsersController],
