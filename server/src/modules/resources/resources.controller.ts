c470cbf094f90b5862825a33c701a53719c94842
Naimul Haque <naimulcsx@gmail.com>
2022-05-21 10:54:05 +0600

    Add Swagger docs for resources

--- server/src/modules/resources/resources.controller.ts
+++ server/src/modules/resources/resources.controller.ts
@@ -11,27 +11,34 @@ import {
   Post,
   UseGuards,
 } from "@nestjs/common"
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger"
 import { ResourcesService } from "./resources.service"
 
 @Controller("resources")
+@ApiTags("resources")
 @UseGuards(IsAuthenticatedGuard)
 export class ResourcesController {
   constructor(private readonly resourceService: ResourcesService) {}
 
   @Post("/")
+  @ApiOperation({ summary: "Create a new resource." })
+  @ApiOkResponse({ description: "Success." })
   async createResources(@Body() body: CreateResourceDto) {
     const newResource = await this.resourceService.createResource(body)
     return { message: "Resource created", data: newResource }
   }
 
   @Get("/")
+  @ApiOperation({ summary: "Get all resources." })
+  @ApiOkResponse({ description: "Success." })
   async getResources() {
     const resources = await this.resourceService.getAllResources()
-
     return { resources }
   }
 
   @Get("/:id")
+  @ApiOperation({ summary: "Get a resource by id." })
+  @ApiOkResponse({ description: "Success." })
   async getAResource(@Param("id") id: number) {
     const resource = await this.resourceService.getAResources(id)
 
@@ -39,6 +46,8 @@ export class ResourcesController {
   }
 
   @Patch("/:id")
+  @ApiOperation({ summary: "Update a resource by id." })
+  @ApiOkResponse({ description: "Success." })
   async updateResource(
     @Body() body: UpdateResourceDto,
     @Param("id") id: number
@@ -47,6 +56,8 @@ export class ResourcesController {
   }
 
   @Delete("/:id")
+  @ApiOperation({ summary: "Delete a resource by id." })
+  @ApiOkResponse({ description: "Success." })
   async deleteResource(@Param("id") id: number) {
     return await this.resourceService.deleteResource(id)
   }
