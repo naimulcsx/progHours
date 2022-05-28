b7acbc255fe808ba1b2d113a702786156e31b02c
Naimul Haque <naimulcsx@gmail.com>
2022-05-28 12:59:43 +0600

    Add form builder

--- client/src/components/AuthContainer.tsx
+++ client/src/components/AuthContainer.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo"
 const AuthContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-light">
-      <div className="w-full max-w-md p-12 mx-auto bg-white rounded-lg shadow">
+      <div className="w-full max-w-md p-8 mx-auto bg-white rounded-lg shadow">
         {/* logo  */}
         <Logo className="text-dark" />
         <div className="mt-6">{children}</div>
