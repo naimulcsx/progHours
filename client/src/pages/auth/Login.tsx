91bdc425864860c760320af773ceff00fc7bb244
Naimul Haque <naimulcsx@gmail.com>
2022-05-28 13:08:51 +0600

    Update login form

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,7 +1,4 @@
-import axios from "axios"
 import * as Yup from "yup"
-import { useFormik } from "formik"
-// import toast from "react-hot-toast"
 import { Helmet } from "react-helmet-async"
 import { Link, useNavigate } from "react-router-dom"
 import toast from "react-hot-toast"
@@ -11,51 +8,13 @@ import toast from "react-hot-toast"
  */
 import showErrorToasts from "@/utils/showErrorToasts"
 import AuthContainer from "@/components/AuthContainer"
-import { FormControl, Input, ErrorMessage, Label } from "@/components/Form"
-
-/**
- * Yup validation schema for login form
- */
-const loginSchema = Yup.object().shape({
-  username: Yup.string()
-    .trim()
-    .required("University ID is required")
-    .length(7, "Invalid University ID"),
-  password: Yup.string().trim().required("Password is required"),
-})
+import FormBuilder from "@/components/FormBuilder"
 
 /**
  * Component for login page
  */
 const Login = (): JSX.Element => {
   const navigate = useNavigate()
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-    },
-    validationSchema: loginSchema,
-    onSubmit: async (values) => {
-      try {
-        /**
-         * TODO: REPLACE THIS WITH REACT-QUERY MUTATION
-         */
-        const { data } = await axios.post("/api/auth/login", values)
-        const { user } = data
-        localStorage.setItem("isLoggedIn", "1")
-        localStorage.setItem("role", "user")
-        localStorage.setItem("userId", user.id)
-        localStorage.setItem("name", user.name)
-        navigate("/dashboard")
-        toast.success("Successfully logged in", { className: "toast" })
-      } catch (error: any) {
-        const { data, status, statusText } = error.response
-        // handle bad gateway errors
-        if (status === 502) toast.error(statusText)
-        showErrorToasts(data.message)
-      }
-    },
-  })
   return (
     <AuthContainer>
       <Helmet>
@@ -70,28 +29,44 @@ const Login = (): JSX.Element => {
           </Link>
         </p>
       </div>
-      <form onSubmit={formik.handleSubmit} className="mt-8 space-y-4">
-        <FormControl
-          isInvalid={formik.touched.username && formik.errors.username}
-        >
-          <Label>University ID</Label>
-          <Input type="text" {...formik.getFieldProps("username")}></Input>
-          <ErrorMessage>{formik.errors.username}</ErrorMessage>
-        </FormControl>
-        <FormControl
-          isInvalid={formik.touched.password && formik.errors.password}
-        >
-          <Label>Password</Label>
-          <Input type="password" {...formik.getFieldProps("password")}></Input>
-
-          <ErrorMessage>{formik.errors.password}</ErrorMessage>
-        </FormControl>
-        <div>
-          <button type="submit" className="block mt-6 btn-primary">
-            Login
-          </button>
-        </div>
-      </form>
+      <FormBuilder
+        className="mt-6 space-y-4"
+        fields={{
+          username: {
+            type: "text",
+            label: "University ID",
+            validate: Yup.string()
+              .trim()
+              .required("University ID is required")
+              .length(7, "Invalid University ID"),
+          },
+          password: {
+            type: "password",
+            label: "Password",
+            validate: Yup.string().trim().required("Password is required"),
+          },
+        }}
+        api="/api/auth/login"
+        onSuccess={(res) => {
+          const { user } = res.data
+          localStorage.setItem("isLoggedIn", "1")
+          localStorage.setItem("role", "user")
+          localStorage.setItem("userId", user.id)
+          localStorage.setItem("name", user.name)
+          navigate("/dashboard") // redirect to dashboard
+          toast.success("Logged in") // create a toast
+        }}
+        onError={(err) => {
+          const { data, status, statusText } = err.response
+          // handle bad gateway errors
+          if (status === 502) toast.error(statusText)
+          showErrorToasts(data.message)
+        }}
+        button={{
+          label: "Login",
+          className: "mt-6",
+        }}
+      />
     </AuthContainer>
   )
 }
