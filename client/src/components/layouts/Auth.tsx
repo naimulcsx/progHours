28972094f32c1cec87c856eaf4063a9318b875c1
shahrierQA <shahrierfahim34@gmail.com>
2022-08-14 01:12:08 +0600

    Add link on logo

--- client/src/components/layouts/Auth.tsx
+++ client/src/components/layouts/Auth.tsx
@@ -1,12 +1,22 @@
-import { Box, Flex,useColorModeValue as mode } from "@chakra-ui/react"
+import { Box, Flex, useColorModeValue as mode } from "@chakra-ui/react"
 import Logo from "../Logo"
+import { Link } from "react-router-dom"
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Flex justify="center" align="center" minH="100vh">
-      <Box p={8} bg={mode("white", "gray.700")} maxW="md" w="full" shadow="base" rounded="lg">
+      <Box
+        p={8}
+        bg={mode("white", "gray.700")}
+        maxW="md"
+        w="full"
+        shadow="base"
+        rounded="lg"
+      >
         {/* logo  */}
-        <Logo />
+        <Link to="/leaderboard">
+          <Logo />
+        </Link>
         <Box mt={4}>{children}</Box>
       </Box>
     </Flex>
