69c12b8148cd280dd0377bc6e441d10d0a504263
MD Nur Uddin Shohan <nuruddin.shohan@yahoo.com>
2022-08-21 13:41:18 +0600

    Problem page

--- client/src/pages/admin/Problem.tsx
+++ client/src/pages/admin/Problem.tsx
@@ -4,9 +4,20 @@ import { useParams } from "react-router-dom"
 import { useQuery } from "react-query"
 import axios from "axios"
 import { useState } from "react"
-useState
+
+import FormBuilder from "@/components/FormBuilder"
+import * as Yup from "yup"
+import {
+  Box,
+  Spinner,
+  useColorModeValue as mode,
+  useToast,
+} from "@chakra-ui/react"
+import { DEFAULT_TOAST_OPTIONS } from "@/configs/toast-config"
+
 export default function Problem() {
   const [probInfo, setProbInfo] = useState(null)
+  const toast = useToast(DEFAULT_TOAST_OPTIONS)
   const { pid } = useParams()
   console.log(pid)
   const getProblemInfo = () => {
@@ -19,7 +30,8 @@ export default function Problem() {
   useQuery("users", getProblemInfo, {
     onSuccess: (res) => {
       console.log(res)
-      setProbInfo(res.body)
+      setProbInfo(res.body.problem)
+      console.log()
     },
   })
 
@@ -29,6 +41,40 @@ export default function Problem() {
       <Helmet>
         <title>{pid}</title>
       </Helmet>
+      {probInfo ? (
+        <FormBuilder
+          fields={{
+            name: {
+              type: "text",
+              label: "Problem Name",
+              validate: Yup.string().trim().required("Name is required"),
+              initialValue: probInfo.name || "",
+            },
+            difficulty: {
+              type: "text",
+              label: "Problem Name",
+              validate: Yup.string().trim().required("Name is required"),
+              initialValue: probInfo.difficulty || "",
+            },
+          }}
+          button={{
+            label: "Update",
+            loadingLabel: "Updating",
+            colorScheme: "gray",
+          }}
+          mutation={(values: any) => {
+            return axios.patch(`/api/problems/${pid}?update=data`, values)
+          }}
+          onSuccess={() => {
+            toast({ status: "success", title: "Account updated!" })
+          }}
+          onError={() => {
+            toast({ status: "error", title: "Some error occurred!" })
+          }}
+        />
+      ) : (
+        <Spinner />
+      )}
     </DashboardLayout>
   )
 }
