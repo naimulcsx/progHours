b1d77cbd155304cd80496ff8f13335b6dfa02f70
Naimul Haque <naimulcsx@gmail.com>
2022-08-28 13:18:42 +0600

    Add service worker in build config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,4 +8,20 @@ export default defineConfig({
     port: 80,
   },
   plugins: [tsconfigPaths(), react()],
+  build: {
+    rollupOptions: {
+      input: {
+        // the default entry point
+        app: "./index.html",
+        "service-worker": "./service-worker.js",
+      },
+      output: {
+        entryFileNames: (assetInfo) => {
+          return assetInfo.name === "service-worker"
+            ? "[name].js" // put service worker in root
+            : "assets/js/[name]-[hash].js" // others in `assets/js/`
+        },
+      },
+    },
+  },
 })
