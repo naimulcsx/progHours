22d0d88d9bb7e1cfe78c14d16202a09b44d5c360
Naimul Haque <naimulcsx@gmail.com>
2023-01-27 06:25:07 +0600

    Fix score fuctions

--- client/src/utils/calculatePoints.ts
+++ client/src/utils/calculatePoints.ts
@@ -1,10 +1,11 @@
 /**
  * Calculate points in frontend from stats that we got from backend
  */
-function calculatePoints(obj: any) {
-  const { totalSolved: y, totalSolveTime: z, totalDifficulty } = obj
-  const x = totalDifficulty / y || 0
-  return (x * y) / 100 + z
+function calculatePoints(item: any) {
+  item.averageDifficulty = item.totalDifficulty / item.totalSolvedWithDifficulty || 0
+  let score = (item.totalSolved * item.averageDifficulty) / 100 + item.totalSolveTime
+  if (!score) score = 0
+  return score
 }
 
 export default calculatePoints
