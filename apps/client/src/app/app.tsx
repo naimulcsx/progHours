17b5057a5a569cd098c061d92397e8a6c6dc902b
Naimul Haque <naimulcsx@gmail.com>
2023-05-30 11:30:26 +0600

    feat: add client application

---
+++ apps/client/src/app/app.tsx
@@ -0,0 +1,38 @@
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <div>
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">progHours</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              progHours <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
