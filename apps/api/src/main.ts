5396a11f44298b85bcd817827546057876633256
Naimul Haque <naimulcsx@gmail.com>
2023-06-02 17:16:06 +0600

    feat(api): setup secure headers with helmet

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,14 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger, ValidationPipe, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { SwaggerTheme } from "swagger-themes";
-
 import { AppModule } from "./app/app.module";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
-  app.setGlobalPrefix(globalPrefix);
+  app.use(helmet());
+  app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe());
   app.enableVersioning({
     type: VersioningType.URI,
@@ -41,9 +36,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port);
 
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/api`);
 }
 
 bootstrap();
