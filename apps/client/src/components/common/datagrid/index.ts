05af08150af5cdb8cd24f32619dc563ee32d0017
Naimul Haque <naimulcsx@gmail.com>
2023-06-28 14:12:51 +0600

    fix(client): update missing imports

--- apps/client/src/components/common/datagrid/index.ts
+++ apps/client/src/components/common/datagrid/index.ts
@@ -1,7 +1,4 @@
-export { ExternalColumnFilter } from "./ColumnFilter";
-export type { ExternalColumnFilterProps } from "./ColumnFilter";
 export { DataGrid, useDataGrid } from "./DataGrid";
-export * from "./filters";
 export type {
   DataGridBodyCellProps,
   DataGridBodyRowProps,
