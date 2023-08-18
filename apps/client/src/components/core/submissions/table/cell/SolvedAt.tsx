e5fe36fb4e8464d0cb85ad044f19f88d592dbdfc
Naimul Haque <naimulcsx@gmail.com>
2023-08-18 20:05:09 +0600

    refactor(client): submissions table

---
+++ apps/client/src/components/core/submissions/table/cell/SolvedAt.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { DatePickerInput } from "@mantine/dates";
+import {
+  SubmissionRow,
+  useUpdateSubmissionMutation
+} from "@proghours/data-access";
+import { useQueryClient } from "@tanstack/react-query";
+import { IconCheck } from "@tabler/icons-react";
+import { notifications } from "@mantine/notifications";
+import moment from "moment";
+import { CellContext } from "@tanstack/react-table";
+
+export function SolvedAtCell(cell: CellContext<SubmissionRow, unknown>) {
+  const queryClient = useQueryClient();
+  const [solvedAt, setSolvedAt] = useState(
+    new Date(cell.row.original.solvedAt)
+  );
+  const { mutate } = useUpdateSubmissionMutation({
+    config: {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["submissions"]);
+        notifications.show({
+          icon: <IconCheck />,
+          color: "green",
+          title: "Success",
+          message: "Updated submission!"
+        });
+      }
+    }
+  });
+  return (
+    <DatePickerInput
+      size="xs"
+      sx={{ minWidth: 100 }}
+      value={solvedAt}
+      valueFormat="DD-MM-YYYY"
+      onChange={(date) => {
+        const value = date || new Date();
+        setSolvedAt(value);
+        mutate({
+          id: cell.row.original.id,
+          solvedAt: moment
+            .utc({
+              year: value?.getFullYear(),
+              date: value.getDate(),
+              month: value.getMonth()
+            })
+            .toDate()
+        });
+      }}
+    />
+  );
+}
---
+++ apps/client/src/components/core/submissions/table/index.tsx
@@ -0,0 +1,120 @@
+import { SubmissionRow } from "@proghours/data-access";
+import {
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable
+} from "@tanstack/react-table";
+import { columns } from "./columns";
+import {
+  Group,
+  Pagination,
+  ScrollArea,
+  Table,
+  Text,
+  useMantineTheme
+} from "@mantine/core";
+import { getBorderColor } from "~/styles/colors";
+
+interface DataTableProps {
+  data: SubmissionRow[];
+}
+
+export function SubmissionsDataTable({ data }: DataTableProps) {
+  const theme = useMantineTheme();
+
+  const PAGE_SIZE = 25;
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    // pagination
+    getPaginationRowModel: getPaginationRowModel(),
+    // initial state
+    initialState: {
+      pagination: {
+        pageSize: PAGE_SIZE
+      }
+    }
+  });
+
+  return (
+    <ScrollArea>
+      <Table fontSize="sm">
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th
+                    key={header.id}
+                    style={{
+                      width: header.column.getSize(),
+                      minWidth: header.column.getSize(),
+                      maxWidth: header.column.getSize()
+                    }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <tr key={row.id} data-state={row.getIsSelected() && "selected"}>
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    key={cell.id}
+                    style={{
+                      width: cell.column.getSize(),
+                      minWidth: cell.column.getSize(),
+                      maxWidth: cell.column.getSize()
+                    }}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+      <Group
+        position="apart"
+        px="xs"
+        py="sm"
+        sx={{ borderTop: `1px solid ${getBorderColor(theme)}` }}
+      >
+        <Text size="sm">
+          {" "}
+          {table.getState().pagination.pageIndex * PAGE_SIZE + 1} -{" "}
+          {Math.min(
+            (table.getState().pagination.pageIndex + 1) * PAGE_SIZE,
+            table.getFilteredRowModel().rows.length
+          )}{" "}
+          / {table.getFilteredRowModel().rows.length}
+        </Text>
+        <Pagination
+          size="sm"
+          boundaries={1}
+          total={table.getPageCount()}
+          onChange={(value) => table.setPageIndex(value - 1)}
+        />
+      </Group>
+    </ScrollArea>
+  );
+}
